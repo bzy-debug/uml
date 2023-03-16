1396e05a17f60eb61c089923a2737e82ac8e0333
bzy-debug <bzy_sustech@foxmail.com>
2023-03-17 02:05:41 +0800

    embed and project

---
+++ src/Convert.hs
@@ -0,0 +1,27 @@
+module Convert where
+
+import Ast
+import Control.Monad (liftM2)
+
+embedInt :: Int -> Value
+embedInt = VNum
+
+projectInt :: Value -> Maybe Int
+projectInt (VNum n) = Just n
+projectInt _ = Nothing
+
+embedBool :: Bool -> Value
+embedBool = VBool
+
+projectBool :: Value -> Bool
+projectBool (VBool False) = False
+projectBool _ = True
+
+embedList :: [Value] -> Value
+embedList [] = VNil
+embedList (v : vs) = VPair (v, embedList vs)
+
+projectList :: Value -> Maybe [Value]
+projectList (VPair (car, cdr)) = liftM2 (:) (Just car) (projectList cdr)
+projectList VNil = Just []
+projectList _ = Nothing
